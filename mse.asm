edaacecedf4c1bfa6c79b92686bbc608b8bb4578
kenedii <55466055+kenedii@users.noreply.github.com>
2025-01-20 19:33:55 -0500

    save sse to memory buffer to use in derivative computation

--- mse.asm
+++ mse.asm
@@ -7,7 +7,9 @@ include \masm32\include\masm32rt.inc
  Y REAL4 -3.0, 1.7, 2.3, 13.4, 4.567, 0.2
             SDWORD -1               ; End of array -> NaN
  Yhat REAL4 -6.0, -1.9, 12.3, 3.4, 4.564, 0.5
-            SDWORD -1               ; End of array -> NaN         
+            SDWORD -1               ; End of array -> NaN   
+
+sse REAL4 0.0      
 
 hi  REAL4 3.14
 bye REAL4 2.71
@@ -28,7 +30,7 @@ start:
 
  mov esi, OFFSET Y    ; true y
  mov edi, OFFSET Yhat ; pred y
- mov ebx, 5           ; # of elements in array
+ mov ebx, 5           ; # of elements in array-1
  call compute_mse
 
  ; Convert REAL4 to string
@@ -40,19 +42,28 @@ start:
 
 
 compute_mse PROC
- ; esi <- pointer to y[0]
- ; edi <- pointer to yhat[0]
+ ; esi <- pointer to y
+ ; edi <- pointer to yhat
  ; ebx <- # of training examples (m)
  ; MSE -> current_sum memory buffer 
+
+ fld 0.0              ; Clear memory buffers in case they have been set already
+ fstp destination
+ fld 0.0 
+ fstp current_sum
+ fld 0.0
+ fstp sse
+ fld 0.0
+ fstp currentIndex
 
  mov endIndex, ebx
 
 
-loopy: ; compute squared error for every item in the array
+loopy:                ; compute squared error for every item in the array
  mov eax, currentIndex
  mov ebx, endIndex
- cmp eax, ebx ; check if we reached the end of array
- jge finish_mse ; if we did, finish up the mse computation
+ cmp eax, ebx         ; check if we reached the end of array
+ jge finish_mse       ; if we did, finish up the mse computation
 
  ; Load y and yhat for current index
  fld REAL4 PTR [esi + eax*4]
@@ -70,15 +81,16 @@ loopy: ; compute squared error for every item in the array
  
  
 
- finish_mse: ; Multiply 1/2m by the sse
-  mov ebx, endIndex
+ finish_mse:          ; Multiply 1/2m by the sse
+  fld current_sum
+  fstp sse            ; Store the sse in memory (we need it for Derivative during Gradient Descent)
 
-  imul ebx, 2 ; compute 2m
+  imul ebx, 2         ; compute 2m
 
   finit
   fld ebx
   fld 1.0
-  fdiv ; compute 1/2m
+  fdiv                ; compute 1/2m
   fld current_sum
   fmul ; compute (1/2m)*(sse) where sse = sum( [y-yhat]^2 ) over all examples
   fstp current_sum ; store the final SSE
