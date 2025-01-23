bf3ea46dd0d2c5a7e7be89cd69b28078e5468cc7
kenedii <55466055+kenedii@users.noreply.github.com>
2025-01-22 19:09:56 -0500

    added se, derivative
    - compute_se PROC to compute the squared error for a single training example
    - compute_dse PROC to compute derivative of squared error function with respect to w or b.

--- mse.asm
+++ mse.asm
@@ -1,30 +1,34 @@
 include \masm32\include\masm32rt.inc
 
 .data
- twom  DWORD 0
- flzero  REAL4 0.0
- flone   REAL4 1.0
+
  X   REAL4 -1.0, 1.2, 2.3, 3.4, 4.567, 0.0
             SDWORD -1        ; End of array -> NaN
  Y REAL4 -3.0, 1.7, 2.3, 13.4, 4.567, 0.2
             SDWORD -1        
  Yhat REAL4 -6.0, -1.9, 12.3, 3.4, 4.564, 0.5
-            SDWORD -1          
+            SDWORD -1  
 
-sse REAL4 0.0      
+ flzero  REAL4 0.0
+ flone   REAL4 1.0
+ fltwo   REAL4 -2.0        
+
+sse REAL4 0.0   
+gradient REAL4 0.0   
 
 destination REAL4 0.0
 current_sum REAL4 0.0
 
+twom  DWORD 0
 currentIndex DWORD 0
 endIndex DWORD 0
-
-formatString BYTE "%f", 0    ; Format string for floating-point output
-buffer db 256 DUP(0)       ; Buffer to hold formatted string
-
 current_sum_qword QWORD 0 ; QWORD buffer for FloatToStr
+example_x DWORD 0
+
 
 newline db " ",13,10,0
+formatString BYTE "%f", 0    ; Format string for floating-point output
+buffer db 256 DUP(0)       ; Buffer to hold formatted string
 
 .code
 
@@ -46,24 +50,40 @@ start:
     ; Print the ASCII result to stdout
     invoke StdOut, ADDR buffer
 
-; Convert REAL4 current_sum to QWORD
-    fld sse
-    fstp current_sum_qword      ; Store the REAL4 value into the QWORD buffer
-
-
-    ; Use FloatToStr to convert current_sum to a string
-    invoke FloatToStr, current_sum_qword, ADDR buffer
-
-    ; Print the ASCII result to stdout
-    invoke StdOut, ADDR buffer
-
     ; Wait for user input before exiting
     invoke StdIn, OFFSET buffer, 256
     ret
 
+compute_se PROC      ; compute se  =  [y-yhat]^2 
+ ; esi <- y
+ ; edi <- yhat
+ ; SE -> current_sum memory buffer 
 
 
-compute_mse PROC
+ fld flzero           ; Clear memory buffers in case they have been set already
+ fstp destination
+ fld flzero
+ fstp current_sum
+ fld flzero
+ fstp sse
+ fld flzero
+ fstp currentIndex
+
+ ; Load y and yhat for current index
+ fld REAL4 PTR [esi]
+ fld REAL4 PTR [edi]
+ fsub                 ; compute y-yhat
+ fld st(0)            ; duplicate the result on the FPU stack
+ fmul                 ; compute (y-yhat)^2
+
+ ; Add the se for current training example to sum
+ fadd current_sum
+ fstp current_sum
+ 
+ret
+compute_se ENDP
+
+compute_mse PROC          ; compute (1/2m)*(sse) where sse = sum( [y-yhat]^2 ) over all examples
  ; esi <- pointer to y
  ; edi <- pointer to yhat
  ; ebx <- # of training examples (m)
@@ -106,8 +126,6 @@ loopy:                ; compute squared error for every item in the array
  
 
  finish_mse:          ; Multiply 1/2m by the sse
-  fld current_sum
-  fstp sse            ; Store the sse in memory (we need it for Derivative during Gradient Descent)
 
   imul ebx, ebx, 2    ; compute 2m
   mov twom, ebx
@@ -118,10 +136,37 @@ loopy:                ; compute squared error for every item in the array
   fdiv                ; compute 1/2m
   fld current_sum
   fmul                ; compute (1/2m)*(sse) where sse = sum( [y-yhat]^2 ) over all examples
-  fstp current_sum    ; store the final SSE
+  fstp current_sum    ; store the final MSE
   
 
  ret
 compute_mse ENDP
+
+
+compute_dse PROC
+ ; eax <-- Value of x[i] (current training example)
+ ; ebx <-- are we calcing deriv w.r.t bias or weight? 0 = bias 1 = weight
+ ; gradient --> gradient memory buffer
+ 
+ mov example_x, eax
+
+ fld flzero
+ fstp gradient    ; reset gradient memory buf
+
+ fld fltwo
+ fld sse          ; -2*squared_error
+ fmul
+
+ cmp ebx, 0       ; If we are computing deriv w.r.t
+ je fin           ; bias, we skip the next steps
+ 
+ fild example_x
+ fmul
+
+fin:
+ fstp gradient
+ 
+ ret
+compute_dse ENDP
 
 end start
