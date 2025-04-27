414f518aa059a90a6c1d14d85494f5700f414c0b
kenedii <55466055+kenedii@users.noreply.github.com>
2025-04-26 23:35:24 -0400

    fix comment

--- train.asm
+++ train.asm
@@ -142,7 +142,7 @@ finish_epoch:
  fstp st(0)                  ; Remove ST(0) (best_mse)
  fstp st(0)                  ; Remove ST(0) (current_sum)
 
- jge update_best_mse         ; Jump if less (if we got a lower mse this time)
+ jge update_best_mse         ; Jump if best mse is greater than current mse (if we got a lower mse this time)
  jmp done                    ; Skip to done
 
 update_best_mse:
